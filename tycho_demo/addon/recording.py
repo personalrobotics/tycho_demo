abc951ede11cf959f2804a5c078aaf9ceae5488e
Kay Ke <4526957+Kelym@users.noreply.github.com>
2023-12-18 15:22:42 -0800

    Clean up camera recording and failure relabeling

--- tycho_demo/addon/recording.py
+++ tycho_demo/addon/recording.py
@@ -20,13 +20,12 @@ from time import strftime, localtime, sleep
 # Singleton
 FNULL = open(os.devnull, 'w')
 ROSBAG_PROC = []
-DEFAULT_CAMERAS = ['kinect']
 
 def add_recording_function(state):
   state.handlers['r'] = _record
   state.handlers['D'] = _delete_recording
   state.handlers['R'] = _count_recording
-  state.handlers['f'] = _failure_label
+  state.handlers['f'] = _relabel_failure_recording
   state.rosbag_recording_to = False
   state.ros_record_topics = [
       '/joint_states', '/joint_commands',
@@ -34,8 +33,10 @@ def add_recording_function(state):
       '/Choppose', '/Choppose_target',
       '/Ball/point', '/R0/point',
       '/R1/point', '/R2/point',
-      '/azcam_front/rgb/image_raw/compressed'
       ]
+  state.ros_record_dicts = {
+    'camera_kinect': '/azcam_front/rgb/image_raw/compressed'
+  }
   state.onclose.append(_stop_recording_on_quit)
 
   if 'save_record_folder' in state.params:
@@ -49,7 +50,7 @@ def add_recording_function(state):
 
 def _record(key, state):
   if state.rosbag_recording_to: # Stop recording if it has been running
-    stop_rosbag_recording()
+    stop_rosbag_recording(state.ros_record_dicts)
     state.rosbag_recording_to = False
   else:                         # Start recording
     rosbag_recording_to = os.path.join(
@@ -57,14 +58,16 @@ def _record(key, state):
       strftime('%y-%m-%d-%H-%M-%S', localtime()))
     state.rosbag_recording_to = True
     state.last_rosbag = rosbag_recording_to
-    start_rosbag_recording(rosbag_recording_to, state.ros_record_topics)
+    start_rosbag_recording(rosbag_recording_to,
+      state.ros_record_topics,
+      state.ros_record_dicts)
 
 def _delete_recording(key, state):
   if state.rosbag_recording_to: # Stop recording
-    stop_rosbag_recording()
+    stop_rosbag_recording(state.ros_record_topics)
     state.rosbag_recording_to = False
   if state.last_rosbag is not None:
-    delete_recording(state.last_rosbag)
+    delete_recording(state.last_rosbag, state.ros_record_topics)
     state.last_rosbag = None
 
 def _count_recording(key, state):
@@ -74,19 +77,18 @@ def _count_recording(key, state):
 
 def _stop_recording_on_quit(state):
   if state.rosbag_recording_to:
-    stop_rosbag_recording()
+    stop_rosbag_recording(state.ros_record_dicts)
 
-
-def _failure_label(key, state):
+def _relabel_failure_recording(key, state):
   if state.rosbag_recording_to: # Stop recording
-    stop_rosbag_recording()
+    stop_rosbag_recording(state.ros_record_dicts)
     state.rosbag_recording_to = False
   if state.last_rosbag is not None:
-    label_failure_demo(state.last_rosbag)
+    label_failure_demo(state.last_rosbag, state.ros_record_dicts)
 
 # ------------------------------------------------------------------------------
 
-def start_rosbag_recording(record_prefix, pose_topics, cameras=DEFAULT_CAMERAS):
+def start_rosbag_recording(record_prefix, pose_topics, dict_topics):
   print_and_cr(colors.bg.green + 'Recording to rosbag {}'.format(
     os.path.basename(record_prefix)))
   args1 = ['rosbag', 'record'] + pose_topics[:-1] + \
@@ -94,19 +96,14 @@ def start_rosbag_recording(record_prefix, pose_topics, cameras=DEFAULT_CAMERAS):
   ROSBAG_PROC.clear()
   ROSBAG_PROC.append(Popen(args1, stdout=FNULL, stderr=STDOUT))
 
-  for _camera in cameras:
-      args2 = ['rosbag', 'record',
-               pose_topics[-1],
-               '-O', record_prefix+'-camera_'+_camera+'.bag',
-               '__name:=camera_'+_camera+'_bag']
-      ROSBAG_PROC.append(Popen(args2, stdout=FNULL, stderr=STDOUT))
-      #args3 = ['rosbag', 'record',
-      #         '/'+_camera+'/aligned_depth_to_color/image_raw/compressedDepth',
-      #         '-O', record_prefix+'-depth_'+_camera+'.bag',
-      #         '__name:=depth_'+_camera+'_bag']
-      #rosbag_writer_cameras.append(Popen(args3, stdout=FNULL, stderr=STDOUT))
+  for topic in dict_topics.keys():
+    args2 = ['rosbag', 'record',
+             dict_topics[topic],
+             '-O', record_prefix+'-'+topic+'.bag',
+             '__name:='+topic+'_bag']
+    ROSBAG_PROC.append(Popen(args2, stdout=FNULL, stderr=STDOUT))
 
-def stop_rosbag_recording(cameras=DEFAULT_CAMERAS):
+def stop_rosbag_recording(dict_topics):
   print_and_cr(colors.bg.lightgrey + f'Stop rosbag recording ({len(ROSBAG_PROC)} subprocs)' + colors.reset)
   for p in ROSBAG_PROC:
     p.terminate()
@@ -115,32 +112,29 @@ def stop_rosbag_recording(cameras=DEFAULT_CAMERAS):
   ROSBAG_PROC.clear()
   args1 = ['rosnode', 'kill', '/pose_bag']
   rosbag_killer = Popen(args1, stdout=FNULL, stderr=STDOUT)
-  for _camera in cameras:
-      args2 = ['rosnode', 'kill', '/camera_'+_camera+'_bag']
-      rosbag_killer = Popen(args2, stdout=FNULL, stderr=STDOUT)
-      #args3 = ['rosnode', 'kill', '/depth_'+_camera+'_bag']
-      #rosbag_killer = Popen(args3, stdout=FNULL, stderr=STDOUT)
+  for topic in dict_topics.keys():
+    args2 = ['rosnode', 'kill', '/'+topic+'_bag']
+    rosbag_killer = Popen(args2, stdout=FNULL, stderr=STDOUT)
 
-def delete_recording(rosbag_recording_to, cameras=DEFAULT_CAMERAS):
+def delete_recording(rosbag_recording_to, dict_topics):
   print_and_cr(colors.bg.red + 'Deleting rosbag recording' + colors.reset)
   list_fn = [rosbag_recording_to+'-pose.bag'] + [
-    rosbag_recording_to+'-camera_'+_camera+'.bag' for _camera in cameras]
+    f'{rosbag_recording_to}-{_topic}.bag' for _topic in dict_topics.keys()]
   for fn in list_fn:
     while not os.path.isfile(fn):
       sleep(0.05)
     os.remove(fn)
 
-def label_failure_demo(rosbag_recording_to, cameras=DEFAULT_CAMERAS):
+def label_failure_demo(rosbag_recording_to, dict_topics):
   print_and_cr(colors.bg.red + 'Re-label rosbag recording' + colors.reset)
   list_fn = [rosbag_recording_to+'-pose.bag'] + [
-    rosbag_recording_to+'-camera_'+_camera+'.bag' for _camera in cameras]
+    f'{rosbag_recording_to}-{_topic}.bag' for _topic in dict_topics.keys()]
   for fn in list_fn:
     while not os.path.isfile(fn):
       sleep(0.05)
     split_fn = fn.split('/')
-    tmp = split_fn[-1]
-    tmp = tmp.split('-')
-    tmp[-1] = 'fail-'+tmp[-1]
-    split_fn[-1] = '-'.join(tmp)
+    new_fn = split_fn[-1].split('-')
+    new_fn[-1] = 'fail-'+new_fn[-1]
+    split_fn[-1] = '-'.join(new_fn)
     cmd = "mv {} {}".format(fn, '/'.join(split_fn))
     os.system(cmd)
