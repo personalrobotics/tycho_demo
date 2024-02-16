e4816794205058cb37f1ed286f7f950226bf5773
Yunchu Zhang <yunchu@g.ucla.edu>
2024-02-15 19:37:30 -0800

    Test launching Azure Camera

--- launch/az_camera.sh
+++ launch/az_camera.sh
@@ -10,5 +10,19 @@ function az_launcher {
     fi
 }
 
-az_launcher "azcam" "roslaunch azure_kinect_ros_driver driver_azcam_front.launch fps:=30 color_resolution:=720P --wait"
-launcher "azcam_undistort" "roslaunch tycho_demo_ros azcam_undistort.launch --wait"
+# For debugging, launch camera without sync (wired_sync_mode:=0 and subordinate_delay_off_master_usec:=0)
+#az_launcher "azcam_side" "roslaunch tycho_demo_ros driver_azcam.launch sensor_sn:=000252514612 camera_name:=azcam_side wired_sync_mode:=0 subordinate_delay_off_master_usec:=0 fps:=30 color_resolution:=720P --wait"
+
+
+az_launcher "azcam_side" "roslaunch tycho_demo_ros driver_azcam.launch sensor_sn:=000252514612 camera_name:=azcam_side wired_sync_mode:=2 subordinate_delay_off_master_usec:=160 fps:=30 color_resolution:=720P --wait"
+launcher "azcam_undistort_side" "ROS_NAMESPACE=azcam_side roslaunch tycho_demo_ros az_camera.launch camera:=azcam_side --wait"
+
+sleep 1
+
+az_launcher "azcam_back" "roslaunch tycho_demo_ros driver_azcam.launch sensor_sn:=001059614112 camera_name:=azcam_back wired_sync_mode:=1 fps:=30 color_resolution:=720P --wait"
+launcher "azcam_undistort_back" "ROS_NAMESPACE=azcam_back roslaunch tycho_demo_ros az_camera.launch camera:=azcam_back --wait"
+
+
+
+az_launcher "azcam_front" "roslaunch tycho_demo_ros driver_azcam.launch sensor_sn:=000704321512 camera_name:=azcam_front wired_sync_mode:=2 subordinate_delay_off_master_usec:=320 fps:=30 color_resolution:=720P --wait"
+launcher "azcam_undistort_front" "ROS_NAMESPACE=azcam_front roslaunch tycho_demo_ros az_camera.launch camera:=azcam_front --wait"
